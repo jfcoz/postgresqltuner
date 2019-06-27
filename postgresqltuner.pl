e16de8e6a91ecb0e0e047b2919ef3d9c1da6b8ed
Julien Francoz <jfcoz@users.noreply.github.com>
2019-06-27 12:30:17 +0200

    Update postgresqltuner.pl

--- postgresqltuner.pl
+++ postgresqltuner.pl
@@ -602,7 +602,7 @@ print_header_1("General instance informations");
 	} else {
 		my $nr_hugepages=get_sysctl('vm.nr_hugepages');
 		if ($nr_hugepages == 0) {
-			print_report_bad("No Huge Pages avaialble on the system");
+			print_report_bad("No Huge Pages available on the system");
 		}
 		if (get_setting('huge_pages') eq 'on') {
 			print_report_ok("huge_pages enabled in PostgreSQL");
