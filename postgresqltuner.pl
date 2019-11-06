9012908fda225fff8c438c14e45b3632f6ceb1fa
Brad Knowles <brad.knowles@wholefoods.com>
2019-11-06 15:13:11 -0600

    Swap '-' and '_' on line 221 to fix regex syntax

--- postgresqltuner.pl
+++ postgresqltuner.pl
@@ -218,7 +218,7 @@ if ($host =~ /^\//) {
 	$os_cmd_prefix='';
 } elsif ($host =~ /^127\.[0-9]+\.[0-9]+\.[0-9]+$/) {
 	$os_cmd_prefix='';
-} elsif ($host =~ /^[a-zA-Z0-9.-_]+$/) {
+} elsif ($host =~ /^[a-zA-Z0-9._-]+$/) {
 	$os_cmd_prefix="ssh $ssh_opts $host ";
 } else {
 	die("Invalid host '$host'");
